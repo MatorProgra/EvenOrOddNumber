44d41bbd41062a65b251ef060a8de7034fb48290
Programator <26222619+MatorProgra@users.noreply.github.com>
2022-02-08 20:16:39 +0100

    New version in .NET 6

---
+++ EvenOrOddNumber/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Program sprawdza parzystość liczb. Koniec - k.");
+
+while (true)
+{
+	Console.Write("Wpisz liczbę: ");
+
+	string? input = Console.ReadLine();
+	if (string.IsNullOrWhiteSpace(input))
+	{
+		ErrorMessage();
+		continue;
+	}
+
+	if (input.ToLower() == "k")
+		break;
+
+	if (!int.TryParse(input, out int evenOrOdd))
+	{
+		ErrorMessage();
+		continue;
+	}
+
+	string result = (evenOrOdd % 2 == 0) ? "parzysta" : "nieparzysta";
+	Console.WriteLine($"Twoja liczba {evenOrOdd} jest {result}.");
+}
+
+Console.WriteLine("\nKoniec programu.");
+Console.ReadLine();
+
+static void ErrorMessage() => Console.Write("Error! Nieprawidłowe dane! ");
